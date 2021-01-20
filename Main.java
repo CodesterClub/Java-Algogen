f38876ca8057caaa73867a428b5704d2f6e9648a
Oogle Glu <peter2003smith@gmail.com>
2021-01-20 20:43:11 +0530

    Updated files

--- Main.java
+++ Main.java
@@ -18,20 +18,20 @@ class Main {
 		String line;												// Store a line of the source
 
 		// For input from terminal
-		console = new BufferedReader( new InputStreamReader( System.in ) );
+		console = new BufferedReader(new InputStreamReader(System.in));
 
 		// Input file path
-		System.out.print( "Enter java file path: " );
+		System.out.print("Enter java file path: ");
 		path = console.readLine();
 
 		// Extract source class name
-		clss = path.substring( path.lastIndexOf( "/" + 1 ), path.lastIndexOf( ".java" ) );
+		clss = path.substring(path.lastIndexOf("/" + 1), path.lastIndexOf(".java"));
 
 		// For source file
-		source = new BufferedReader( new FileReader( path ) );
+		source = new BufferedReader(new FileReader(path));
 
 		// For algo file
-		algo = new PrintWriter( "Algos/" + clss + "_Algo.txt" );
+		algo = new PrintWriter("Algos/" + clss + "_Algo.txt");
 
 		// Initialisatopn of StatementType and StatementConvert objects
 		sT = new StatementType();
@@ -44,22 +44,22 @@ class Main {
 		 * their if blocks should be kept seperate. That is why class and function 
 		 * declarations are not handled by a single block.
 		 */
-		while ( ( line = source.readLine() ) != null ) {
+		while((line = source.readLine()) != null) {
 			// Import declarations
-			if ( sT.importDeclaration( line ) ) {
-				algo.println( line );
+			if(sT.importDeclaration(line)) {
+				algo.println(line);
 			}
 			// Class declarations
-			else if ( sT.classDeclaration( line ) ) {
-				algo.println( line.replace( "{", ":" ).replace( "extends", "inherits" ) );
+			else if(sT.classDeclaration(line)) {
+				algo.println(line.replace("{", ":").replace("extends", "inherits"));
 			}
 			// Function declarations
-			else if ( sT.functionDeclaration( line ) ) {
-				algo.println( line.replace( "{", ":" ).replace( "extends", "inherits" ) );
+			else if(sT.functionDeclaration(line)) {
+				algo.println(line.replace("{", ":").replace("extends", "inherits"));
 			}
 			// If no module available
 			else {
-				 algo.println( line );
+				 algo.println(line);
 			}
 		}
 	}
